62f2413d4f6cc1b40812c736dc0ec59a1389df8b
Ciano456 <cianoconnor713@gmail.com>
2026-07-31 15:43:39 +0100

    fix: prevent stale lookup and page cache regressions

--- smartresolve/settings/base.py
+++ smartresolve/settings/base.py
@@ -144,6 +144,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "accounts.middleware.AuthenticatedNoStoreMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
